612a5bc9d19934aeefdb744659f6bba159978e08
Filippo Gallizia <galliziafilippo@gmail.com>
2020-07-09 10:03:07 +0100

    3 ex in progress

--- exercises/W02/EX03.ANSWER.js
+++ exercises/W02/EX03.ANSWER.js
@@ -1,7 +1,3 @@
 const peopleArr = ["alice", "filippo", "tim", "bryony", "andrea", "luciano"];
 const agesArr = [28, 30, 33, 33, 40, 0.3];
 
-
-const Arr = for (let a in peopleArr){
-	console.log (a);
-}
