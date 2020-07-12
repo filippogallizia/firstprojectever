9265eedc5d2fb81343d7be7a1c944e44cf3f27ab
Tim Kock <me@timkock.com>
2020-07-12 16:24:34 +0100

    feat(#1): nasty strings exercise

--- exercises/W03/E04.js
+++ exercises/W03/E04.js
@@ -3,17 +3,33 @@
 /////////////////
 
 // concepts:
-//  - 
+//  - String.substr, String.length, String.toUpperCase, String.indexOf, Array.reduce ternary operator, string templates
 // task:
-//  - 
+//  - The task is to output the array of strings exactly as is shown in the desired output.
 // hint:
-//  - 
+//  - The hints are in the concepts to be covered.
 
-CODE
+const caparezza = ['Sono', 'fuori', 'dal', 'tunnel,', 'Del', 'divertimento,', 'Sono', 'fuori', 'dal', 'tunnel,', 'Del', 'divertimento,', 'Quando', 'esco', 'di', 'casa', 'e', 'mi', 'annoio,', 'sono', 'molto', 'contento,', 'Quando', 'esco', 'di', 'casa', 'e', 'mi', 'annoio,', 'sono', 'molto', 'più', 'contento...', 'Di', 'te', 'che', 'spendi', 'stipendi,', 'stipato', 'in', 'posti', 'stupendi,', 'Tra', 'culi', 'su', 'cubi,', 'Succubi', 'di', 'beat', 'orrendi,', 'Succhi', 'brandy', 'e', 'ti', 'stendi,', 'Dandy', 'non', 'mi', 'comprendi,', 'senti', 'tu', 'non', 'ti', 'offendi,', 'Se', 'ti', 'dico', 'che', 'sei', 'trendy,', 'Prendi', 'me', 'per', 'esempio,', 'Non', 'mi', 'stempio'];
 
-// desired output:
+// desired (hint: exact, spacing, cases, order) output:
 /*
-
+*************************************
+* Sono fuori dal tunnel,            *
+* Del divertimento,                 *
+* Sono fuori dal tunnel,            *
+* Del divertimento,                 *
+* Quando esco di casa e mi annoio,  *
+* sono molto contento,              *
+* Quando esco di casa e mi annoio,  *
+* sono molto più contento...        *
+* Di te che spendi stipendi,        *
+* Stipato in posti stupendi,        *
+* Tra culi su cubi,                 *
+* Succubi di beat orrendi,          *
+* Succhi brandy e ti stendi,        *
+* Dandy non mi comprendi,           *
+* Senti tu non ti offendi,          *
+* Se ti dico che sei trendy,        *
+* Prendi me per esempio,            *
+*************************************
 */
-
-
