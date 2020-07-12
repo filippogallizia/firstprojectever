321121b5ca0bc25b44e9c22dcec80c1422b1bb2a
Tim Kock <me@timkock.com>
2020-07-12 19:51:21 +0100

    feat(#1): exercise 05, build a ceasar cipher

--- exercises/W03/E05.js
+++ exercises/W03/E05.js
@@ -3,17 +3,26 @@
 /////////////////
 
 // concepts:
-//  - 
+//  - %, String.fromCharCode, String.charCodeAt, ascii table, (regex if you want to get fancy)
 // task:
-//  - 
+//  - Write a Ceasar Cipher that can encrypt and decrypt (alphanumeric only)
 // hint:
-//  - 
+//  - [Ceasar Cipher](https://en.wikipedia.org/wiki/Caesar_cipher)
 
-CODE
+const plaintext = 'this is an ultra secret message, my pincode is 3452';
+
+function ceasarCypher (str, shift) {
+  /* build */
+}
+
+const encrypted = ceasarCypher(plaintext, 12);
+console.log('encrypted', encrypted);
+const decrypted = ceasarCypher(encrypted, -12);
+console.log('decrypted', decrypted);
+
 
 // desired output:
 /*
-
+encrypted ftue ue mz gxfdm eqodqf yqeemsq, yk buzoapq ue 5674
+decrypted this is an ultra secret message, my pincode is 3452
 */
-
-
